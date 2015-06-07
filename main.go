57471db206b6eb6734ef39efcb053ce6d166118e
Bryan Burke <btburke@fastmail.com>
2015-06-07 07:55:44 +0000

    Add CORS middleware

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import "github.com/gin-gonic/gin"
 import "github.com/BTBurke/gaea-server/routes"
+import "github.com/BTBurke/gaea-server/middleware"
 
 func main() {
     r := gin.Default()
+    r.Use(middleware.CORS())
     
     r.GET("/ping", func(c *gin.Context) {
         c.String(200, "pong")

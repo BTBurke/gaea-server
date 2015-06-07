fc5d1a65f520243e2899a71480517351a7654932
Bryan Burke <btburke@fastmail.com>
2015-06-07 06:05:57 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+    r := gin.Default()
+    
+    r.GET("/ping", func(c *gin.Context) {
+        c.String(200, "pong")
+    })
+
+    r.Run(":9000")
+}

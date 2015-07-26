b8df66c14d60c86046d0b4bf35eca4deeea49d8d
Bryan Burke <bryan.t.burke@gmail.com>
2015-07-26 02:38:04 +0000

    add routes for update, list all users, delete user

--- routes/user.go
+++ routes/user.go
@@ -40,3 +40,72 @@ func GetCurrentUser(db *sqlx.DB) gin.HandlerFunc {
 		c.JSON(200, user1)
 	}
 }
+
+func GetAllUsers(db *sqlx.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// TODO: Add check for role admin or higher
+		
+		var users = []User{}
+		
+		err := db.Select(&users, "SELECT * FROM gaea.user")
+		if err != nil {
+			fmt.Println(err)
+			c.AbortWithError(503, errors.APIError{503, "failed on getting all users", "failed to get all users"})
+			return
+		}
+		
+		c.JSON(200, gin.H{"qty": len(users), "users": users})
+		
+	}
+}
+
+func UpdateUser(db *sqlx.DB) gin.HandlerFunc {
+	return func (c *gin.Context) {
+		
+		var user1 User
+		err := c.Bind(&user1)
+		if err != nil {
+			fmt.Println(err)
+			c.AbortWithError(422, errors.APIError{422, "failed on updating user", "failed to update user"})
+			return
+		}
+		
+		var retU User
+		dbErr := db.Get(&retU, `UPDATE gaea.user SET 
+		first_name = $1, last_name = $2, email = $3,
+		dip_id = $4, passport = $5, section = $6, updated_at = $7
+		WHERE user_name = $8 RETURNING *`,
+		user1.FirstName, user1.LastName, user1.Email,
+		user1.DipID, user1.Passport, user1.Section, time.Now(),
+		user1.UserName)
+		
+		if dbErr != nil {
+			fmt.Println(dbErr)
+			c.AbortWithError(503, errors.APIError{503, "failed on updating user", "failed to update user"})
+			return		
+		}
+		
+		c.JSON(200, retU)
+	}
+}
+
+func DeleteUser(db *sqlx.DB) gin.HandlerFunc {
+	return func (c *gin.Context) {
+		var user1 User
+		err := c.Bind(&user1)
+		if err != nil {
+			fmt.Println(err)
+			c.AbortWithError(422, errors.APIError{422, "failed on deleting user", "failed to delete user"})
+			return
+		}
+		
+		dbErr := db.MustExec(`DELETE gaea.sale WHERE user_name = $1`, user1.UserName)
+		if dbErr != nil {
+			fmt.Println(dbErr)
+			c.AbortWithError(503, errors.APIError{503, "failed on updating user", "failed to update user"})
+			return		
+		}
+		
+		c.JSON(200, gin.H{"user_name": user1.UserName})
+	}
+}

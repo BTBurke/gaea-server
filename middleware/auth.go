6474002c49a01f44bf8584aaba543bccca5208df
Bryan Burke <bryan.t.burke@gmail.com>
2015-09-01 08:56:37 +0000

    add logging for failed JWT

--- middleware/auth.go
+++ middleware/auth.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/BTBurke/gaea-server/routes"
+	"github.com/BTBurke/gaea-server/log"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,18 +12,21 @@ func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		jwtComplete := c.Request.Header.Get("Authorization")
 		if len(jwtComplete) == 0 {
+			log.Error("failed to receive JWT dev=no JWT in header")
 			c.AbortWithStatus(401)
 			return
 		}
 
 		jwtString := strings.Split(jwtComplete, " ")
 		if len(jwtString) != 2 || jwtString[0] != "Bearer" {
+			log.Error("failed to receive JWT dev=JWT not right format")
 			c.AbortWithStatus(401)
 			return
 		}
 
 		token, err := routes.ValidateJWT(jwtString[1])
 		if err != nil {
+			log.Error("failed to validate JWT dev=JWT failed validation err=%s", err)
 			c.AbortWithStatus(401)
 			return
 		}
@@ -35,6 +39,7 @@ func Auth() gin.HandlerFunc {
 
 		newJwt, err := routes.RenewJWTfromJWT(jwtString[1])
 		if err != nil {
+			log.Error("failed to validate JWT dev=JWT failed validation err=%s", err)
 			c.AbortWithStatus(401)
 			return
 		}

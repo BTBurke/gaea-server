240b36b8c493600e3e8308a0e96faa46faf065f6
Bryan Burke <bryan.t.burke@gmail.com>
2015-09-08 19:14:40 +0800

    use bolt instead of redis

--- routes/jwt.go
+++ routes/jwt.go
@@ -3,12 +3,12 @@ package routes
 import (
 	"crypto/rand"
 	"fmt"
+	"log"
 	"os"
-	"strings"
 	"time"
 
+	"github.com/boltdb/bolt"
 	"github.com/dgrijalva/jwt-go"
-	"gopkg.in/redis.v3"
 )
 
 const TOKEN_EXPIRE_HRS = 24
@@ -96,37 +96,76 @@ func ValidateJWT(inToken string) (*jwt.Token, error) {
 	return nil, err
 }
 
+// func lookupSecret(user string) ([]byte, error) {
+// 	addr := os.Getenv("JWT_PORT_6379_TCP_ADDR")
+// 	if len(addr) == 0 {
+// 		addr = "127.0.0.1"
+// 	}
+// 	fullAddr := strings.Join([]string{addr, "6379"}, ":")
+// 	fmt.Printf("Using redis connection: %s\n", fullAddr)
+//
+// 	client := redis.NewClient(&redis.Options{
+// 		Addr:     fullAddr,
+// 		Password: "",
+// 		DB:       0,
+// 	})
+//
+// 	secKey := strings.Join([]string{"user:", user, ":secret"}, "")
+//
+// 	ttl, _ := client.TTL(secKey).Result()
+// 	secret, err := client.Get(secKey).Bytes()
+// 	if err == redis.Nil || ttl < 3*time.Hour {
+// 		fmt.Printf("New secret for %s\n", user)
+// 		b, err := makeRandomKey()
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		if err := client.Set(secKey, b, 8*time.Hour).Err(); err != nil {
+// 			return nil, err
+// 		}
+// 		return b, nil
+// 	}
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return secret, nil
+// }
+
 func lookupSecret(user string) ([]byte, error) {
-	addr := os.Getenv("JWT_PORT_6379_TCP_ADDR")
-	if len(addr) == 0 {
-		addr = "127.0.0.1"
+	boltDB := os.Getenv("BOLT_DB")
+	if len(boltDB) == 0 {
+		log.Fatal("Must set bolt DB location in environment BOLT_DB")
 	}
-	fullAddr := strings.Join([]string{addr, "6379"}, ":")
-	fmt.Printf("Using redis connection: %s\n", fullAddr)
-	
-	client := redis.NewClient(&redis.Options{
-		Addr:     fullAddr,
-		Password: "",
-		DB:       0,
-	})
-
-	secKey := strings.Join([]string{"user:", user, ":secret"}, "")
-
-	ttl, _ := client.TTL(secKey).Result()
-	secret, err := client.Get(secKey).Bytes()
-	if err == redis.Nil || ttl < 3*time.Hour {
-		fmt.Printf("New secret for %s\n", user)
-		b, err := makeRandomKey()
-		if err != nil {
-			return nil, err
-		}
-		if err := client.Set(secKey, b, 8*time.Hour).Err(); err != nil {
-			return nil, err
-		}
-		return b, nil
-	}
+	db, err := bolt.Open(boltDB, 0600, &bolt.Options{Timeout: 2 * time.Second})
 	if err != nil {
 		return nil, err
+	}
+	var secret []byte
+	boltErr := db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("secret"))
+		if err != nil {
+			return err
+		}
+
+		value := b.Get([]byte(user))
+		switch {
+		case value == nil:
+			newSecret, err := makeRandomKey()
+			if err != nil {
+				return err
+			}
+			if err := b.Put([]byte(user), newSecret); err != nil {
+				return err
+			}
+			copy(secret, newSecret)
+			return nil
+		default:
+			copy(secret, value)
+			return nil
+		}
+	})
+	if boltErr != nil {
+		return nil, boltErr
 	}
 	return secret, nil
 }
